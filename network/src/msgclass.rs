eef3a4c7dec64fa0d8160f5208b275f48f8304ec
Gokul Ravindran <neohex262@gmail.com>
2017-12-08 23:14:15 +0530

    modify miner

---
+++ network/src/msgclass.rs
@@ -0,0 +1,10 @@
+use chain::block::Block;
+use chain::transaction::SignedTransaction;
+use util::hash::H256;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum MsgClass {
+    BLOCK(Block),
+    SYNCREQ(H256),
+    TX(SignedTransaction),
+MSG(Vec<u8>)
