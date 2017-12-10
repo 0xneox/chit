c5e875da2e48be0cb484985007d52841b2c6a337
Gokul Ravindran <neohex262@gmail.com>
2017-12-10 07:07:39 +0530

    add pbc

---
+++ utli/sha3/build.rs
@@ -0,0 +1,8 @@
+extern crate gcc;
+
+fn main() {
+    gcc::Build::new()
+                .file("src/tinykeccak.c")
+                .static_flag(true)
+                .compile("libtinykeccak.a");
+}
