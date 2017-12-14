8bb914c2f68882a289a9d8afde9e2598507c73df
Gokul Ravindran <neohex262@gmail.com>
2017-12-14 17:14:39 +0530

    Update and rename libs.rs to lib.rs

--- chain/src/lib.rs
+++ chain/src/lib.rs
@@ -1,14 +1,17 @@
 extern crate util;
 extern crate crypto;
 extern crate serde;
+
 #[macro_use]
 extern crate serde_derive;
 extern crate parking_lot;
 extern crate rand;
+
 #[macro_use]
 extern crate log;
 extern crate bincode;
 extern crate bls;
+
 #[macro_use]
 extern crate rlp_derive;
 extern crate rlp;
@@ -16,7 +19,6 @@ extern crate kvdb;
 extern crate heapsize;
 extern crate bigint;
 extern crate ethcore_bytes as bytes;
-
 pub mod error;
 pub mod block;
 pub mod chain;
